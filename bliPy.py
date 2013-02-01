9e67cc07c9ea473ce6b35f304fde9c866eb5ecfc
DPontes <diogo@diogo.(none)>
2013-02-01 19:36:05 +0100

    small correction

--- bliPy.py
+++ bliPy.py
@@ -3,19 +3,20 @@
 import time, sys, os, subprocess
 
 path = '/home/diogo/i386/blink1/commandline/blink1-tool'
-cmd_on = ' --on'
-cmd_off = ' --off'
-cmd_red = ' --red'
-cmd_green = ' --green'
-cmd_blue = ' --blue'
-cmd_rgb = ' --rgb '
-cmd_blink = ' --blink '
-cmd_random = ' --random '
-cmd_list = ' --list'
+
+cmd_on      = ' --on'
+cmd_off     = ' --off'
+cmd_red     = ' --red'
+cmd_green   = ' --green'
+cmd_blue    = ' --blue'
+cmd_rgb     = ' --rgb '
+cmd_blink   = ' --blink '
+cmd_random  = ' --random '
+cmd_list    = ' --list'
 cmd_version = ' --version'
 
-opt_fade = ' -m ' # written in milliseconds
-opt_timing = ' -t ' # written in milliseconds
+opt_fade    = ' -m ' # written in milliseconds
+opt_timing  = ' -t ' # written in milliseconds
 
 time_interval = 1.75
 
@@ -32,7 +33,7 @@ class Blink1:
 		subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
 		time.sleep(interval)
 
-		command = path + cmd_fade + str(interval * 1000) + cmd_on
+		command = path + opt_fade + str(interval * 1000) + cmd_on
 		subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
 		time.sleep(interval)
 
